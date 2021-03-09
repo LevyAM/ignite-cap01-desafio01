1c4748b4253b5a708404fadadaee2cf14f110c9b
LevyAM <levyzudo@msn.com>
2021-03-08 23:06:57 -0300

    Construindo as rotas dos 'todos'

--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  })
+
+  return response.status(201).send("Usuário criado com sucesso");
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {username} = request.headers;
+
+  return response.status(200).json(username.todos);
+
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
